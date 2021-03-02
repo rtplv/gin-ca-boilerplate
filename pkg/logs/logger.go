576a22bf5aec5dc330bcd1e8746e83222818fe91
Ruslan Tupolev 🛫 <rtplv@yandex.ru>
2021-03-02 16:55:38 +0300

    Add logger interface. Caller with depth 4

--- pkg/logs/logger.go
+++ pkg/logs/logger.go
@@ -6,36 +6,41 @@ import (
 	"os"
 )
 
-type Logger struct {
+type Logger interface {
+	Info(message string)
+	Error(err error)
+	Fatal(err error)
+}
+
+type JSONLogger struct {
 	jsonLogger log.Logger
 }
 
-func NewLogger() *Logger {
+func NewLogger() Logger {
 	jsonLogger := log.NewJSONLogger(os.Stdout)
 	jsonLogger = log.With(jsonLogger, "ts", log.DefaultTimestampUTC)
-	jsonLogger = log.With(jsonLogger, "caller", log.DefaultCaller)
+	jsonLogger = log.With(jsonLogger, "caller", log.Caller(4))
 
-	return &Logger{
+	return &JSONLogger{
 		jsonLogger: jsonLogger,
 	}
 }
 
-func (l Logger) Info(message string) {
+func (l JSONLogger) Info(message string) {
 	logErr := l.jsonLogger.Log("type", "info", "message", message)
 	if logErr != nil {
 		fmt.Println(logErr, message)
 	}
 }
 
-func (l Logger) Error(err error)  {
+func (l JSONLogger) Error(err error) {
 	logErr := l.jsonLogger.Log("type", "error", "message", err.Error())
 	if logErr != nil {
 		fmt.Println(logErr, err)
 	}
 }
 
-
-func (l Logger) Fatal(err error)  {
+func (l JSONLogger) Fatal(err error) {
 	logErr := l.jsonLogger.Log("type", "fatal", "message", err.Error())
 	if logErr != nil {
 		fmt.Println(logErr, err)
